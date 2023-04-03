c172e8dcdc08994ee226777de15f0370afa49c6a
JP100108 <100318527+JP100108@users.noreply.github.com>
2023-04-03 19:06:27 -0300

    Add files via upload

---
+++ receitas.js
@@ -0,0 +1,202 @@
+{
+ cafes:{
+    Latte:{
+      "ingredientes"
+      "café solúvel extra forte"
+      "açúcar a gosto"
+      "200 ml água fervendo"
+      "200 ml leite"
+      "cobertura de sorvete"
+      "modo de Fazer"
+      "Faça o café na xícara, de modo que ocupe apenas 1/3 da xícara e adoce a seu gosto."
+      "Esquente o leite até 70° C que é um pouco antes de ferver (quando começar a formar bolinhas ao redor da leiteira)."
+      "Coloque o leite em uma prensa francesa e agite ou se não tiver coloque em uma garrafa pet de 600 ml e agite por uns 3 minutos (cuidado com a pressão ao abrir)."
+      "Abra e deixe uns 30 segundos sobre uma superfície plana e, em seguida, incline suavemente de forma que caia só o leite mais aquoso na xícara, deixando a espuma por último."
+      "Derrame a espuma e, em seguida, faça desenhos com a cobertura de chocolate."
+
+    };
+    Mocca:{
+      "ingredientes"
+      "Café especial (20g)"
+      "Água quente (100g)"
+      "Leite (100ml)"
+      "Doce de Leite (40g)"
+      "PASSO 1"
+    "Mocaccino com doce de leitePrimeiramente, acrescente os 40g de Doce de Leite, já no recipiente que você irá "
+   " servir seu drink Mocha. Nivele e espalhe bem o doce no fundo do recipiente, preparando-o para receber o leite cremoso."
+    "Atenção! Sempre faça esse procedimento antes de coar o café. Isso porque enquanto o café está sendo filtrado, vai ocorrer a "
+    "decantação no leite que será adicionado posteriormente, onde o maior percentual de líquido vai para o fundo do recipiente e a espuma para o topo. "
+    "Dessa forma, o drink terá um degradê mais bonito e apresentável."
+
+   "Dica: como a bebida é servida quente, o ideal é que o recipiente escolhido tenha um apoio, ou seja, uma alça. Dessa forma, você não corre o "
+   "risco de queimar as mãos se o líquido estiver muito quente. Para essa receita, por exemplo, optamos pela taça conhecida como hot drink."
+
+     "PASSO 2"
+    "Leite cremoso para receita do drink mocaccino"
+    "Agora é o momento de preparar o leite cremoso. Já sabe como vai fazer o seu?"
+
+   "• Utilizando o Espumador de Leite da empresa Presca;"
+   "• Ou a Prensa Francesa."
+
+   "Importante! A temperatura ideal para que seu drink tenha uma boa harmonização com o café não pode ultrapassar 70ºC."
+
+   "Dica: caso você não tenha um termômetro para medir a temperatura do leite, de forma geral, quando ele se aproxima da fervura, significa que ele está no ponto ideal."
+
+     "PASSO 3"
+   "Melhor método de fazer leite cremoso para o drink mocha"
+   "Em seguida, acrescente o leite sob o doce de leite, deixando espaço para receber o café."
+    }
+   }
+ };
+
+series:{
+   the_flash:{
+      "Ingredientes "
+      "Café:"
+      
+      "30g.	Café solúvel"
+      "190g.	Açúcar"
+      "120 ml.	Água"
+      "Espuma de leite:"
+      
+      "O necessário	Leite Gelado"
+      "1 pedra	Gelo"
+      "Montagem:"
+      
+      "¾ xícara	Leite"
+     " O necessário	Canela em pó"
+      "Modo de preparo"
+      
+      "Café: bata tudo na batedeira, de 10 a 15 minutos. Ele vai crescer e parecer um merengue. O que não usar da primeira vez "
+      "guardar em um pote no congelador e ir usando aos poucos, lá ele vai ficar consistente, mas “cremoso” e só pegar uma colher "
+      "e misturar no leite quente."
+      
+      "Espuma de Leite: em um pote de vidro coloque o leite e a pedra de gelo. Cerca de um dedo de leite para um pote"
+       "de cinco dedos, já que ele cresce. Tampe bem o pote e chacoalhe ele até espumar e a pedra de gelo se dissolver. Retire a tampa e coloque "
+       "no micro-ondas por aproximadamente 30 segundos para a espuma se estabilizar, retire delicadamente com uma colher."
+      "Montagem: esquente o leite e adicione o café, misture bem e coloque a espuma por cima. Para o raio desenhe-o em uma folha de "
+      "papel manteiga, recorte e coloque a parte vazada em cima da espuma e com uma peneira polvilhe a canela."
+
+      };
+
+   cerverja_amanteigada:{
+      "Ingredientes"
+      "500 ml de SORVETE DE CREME OU BAUNILHA (1/4 do pote de 2 litros)"
+      "100 gr de MANTEIGA em temperatura ambiente (ou meia xícara)"
+      "1 xícara (chá) de AÇÚCAR MASCAVO"
+      "1/2 colher (sopa) de CANELA EM PÓ"
+      "1/2 colher (café) de CRAVO EM PÓ"
+      "NÓZ MOSCADA e GENGIBRE EM PÓ à gosto"
+      "1 garrada de ESPUMANTE (mais ou menos 700 ml) "  
+      "Modo de Fazer"
+      "Misture bem a manteiga com o açúcar e os condimentos, acrescente o sorvete e misture mais."
+      "Leve ao freezer até congelar."
+      "Na hora de servir, coloque 2 a 3 colheres da mistura congelada em um copo, esquente o espumante até começar a ferver, "
+      "(o álcool vai evaporar) e despeje em cima, depois mexa até incorporar tudo."
+      "Aguarde um pouco e o creme vai subir formando o colarinho do chopp."
+   }
+
+};
+
+drinksCA:{
+   Martini:{
+      "INGREDIENTES"
+     " 3 doses de gim (150 ml)"
+     " ½ dose de vermute seco (25 ml)"
+      "cubos de gelo"
+      "casquinha de limão siciliano para perfumar"
+      "1 azeitona para decorar"
+
+      "MODO DE PREPARO"
+     "1. Num copo misturador cheio de gelo, junte o gim e o vermute. Mexa em círculos com uma colher bailarina (sem partir demais o gelo) até a mistura estar bem gelada."
+
+    " 2. Apoie uma peneira para coquetelaria na borda de uma taça para Martini e despeje a mistura. Com as mãos, torça a casca de limão siciliano" 
+     "(isto ajuda a liberar seus óleos) na borda da taça."
+
+     "3. Decore com a azeitona e sirva a seguir."
+
+   Sex_on_the_beach:{
+      "1 dose de vodka"
+      "1/2 dose de licor de pêssego"
+      "1 dose de suco de laranja"
+      "3 cubos de gelo"
+      "1/2 dose de xarope de groselha"
+      
+      "Em uma coqueteleira, coloque a vodka, o licor, o suco de laranja e bata."
+      "Adicione o gelo e misture."
+      "Separe um copo de drink grande e despeje metade da groselha."
+      "Transfira a mistura da coqueteleira para o copo e finalize com o restante da groselha."
+
+   }  
+   }
+};
+drinksNA:{
+   
+ BatidaAbaCMoran:{
+   "ingredientes"
+
+   "200 gramas de abacaxi picado"
+   "100 gramas de morangos congelados"
+   "1 xícara de chá de sorvete de creme"
+   "½ xícara de chá de água"
+   "1 litro de refrigerante de limão}"
+   "Modo de Preparo"
+   "Em um liquidificador, coloque o abacaxi picado, os morangos, o sorvete de creme, a água e bata tudo até que fique homogêneo."
+   "Para servir, coloque ¼ da batida e 200 ml do refrigerante de limão."
+
+
+
+};
+
+Blue_drink:{
+        "Ingredientes"
+     "½ lata de leite condensado"
+     "600 ml de refrigerante de limão"
+     "Corante alimenticio na cor azul"
+     "Folhas de hortelã e gelo a gosto"
+
+        "Modo de preparo     "
+     "No liquidificador, coloque todos os ingredientes, exceto o gelo e as folhas de hortelã."
+     "Bata por mais ou menos cinco minutos - lembre-se de que o corante deve ser utilizado em poucas quantidades"
+     "No copo, coloque gelo, as folhas de hortelã, acrescente a bebida e sirva gelada"
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
